97d5cb64bcbc95908e842c76f62a41fa82eb175c
Taylor <t.richardson@live.ca>
2020-03-29 17:00:18 -0400

    Test

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://www.taylorrichardson.com/',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: "Taylor's Portfolio",
+  subtitle: 'Leader and developer working out of Oshawa, Ontario.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
-  postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  postsPerPage: 5,
+  googleAnalyticsId: 'UA-162140194-1',
   useKatex: false,
   menu: [
     {
@@ -25,19 +25,19 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Taylor Richardson',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'Leader and developer working out of Oshawa, Ontario.',
     contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
+      email: 't.richardson@live.ca',
+      facebook: '',
+      telegram: '',
+      twitter: '',
+      github: 'taylorrichardson',
       rss: '',
       vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
+      linkedin: 'taylorjamesrichardson',
+      instagram: '',
       line: '',
       gitlab: '',
       weibo: '',
